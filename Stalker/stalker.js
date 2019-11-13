2a91d1bc727bc5ce3acb5b43e79f322495a7f401
Anusha <potluri.anusha1@gmail.com>
2019-11-13 20:31:29 +0530

    Stalker_Js

---
+++ Stalker/stalker.js
@@ -0,0 +1,4 @@
+var firstName = prompt("what is your first name?");
+var lastName = prompt("what is your last name?");
+var age = prompt("what is your age?");
+console.log('My full name is ' + firstName + " "+ lastName + "\n" + "My age is" + " " +age);
