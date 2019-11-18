868d4a724246054df3abef07cbe4fed83d7c3117
Anusha <potluri.anusha1@gmail.com>
2019-11-18 20:51:26 +0530

    movie DB

--- Object/movieObject.js
+++ Object/movieObject.js
@@ -1,22 +1,31 @@
 var movieArr = [
     {
         name: "abc",
-        rating:2,
+        rating:5,
         hasWatched: true
     },
     {
         name: "absdc",
-        rating:2,
-        hasWatched: true
+        rating:4.5,
+        hasWatched: false
     },
     {
         name: "abdec",
-        rating:2,
+        rating:3,
         hasWatched: true
     },
     {
         name: "abcace",
-        rating:2,
-        hasWatched: true
+        rating:4.5,
+        hasWatched: false
     },
 ];
+//console.log(movieArr.length);
+for(var i=0;i<movieArr.length;i++){
+if(movieArr[i].hasWatched){
+    console.log("you have watched" + movieArr[i].name + "-" + movieArr[i].rating);
+}
+else{
+    console.log("you have not seen" + movieArr[i].name + "-" + movieArr[i].rating);
+}
+}
