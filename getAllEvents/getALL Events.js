cda362b4a7127cc34467769a255f669567c24a72
Anusha <potluri.anusha1@gmail.com>
2019-11-26 19:58:47 +0530

    getelemnts

--- getAllEvents/getALL Events.js
+++ getAllEvents/getALL Events.js
@@ -1,22 +1,29 @@
 //get list of all tables
 var listOfTable = document.getElementsByTagName('table');
-console.log(listOfTable);
+console.log(listOfTable.length);
 //get children of each table
 var childrenOfTable = document.getElementsByTagName('table')[0].children[1];
 console.log(childrenOfTable);
 //identify th
 var getth = document.getElementsByTagName('table')[0].children[1].children[0].children[0];
-console.log(getth);
+//console.log(getth);
 //if th text = event name
 var thText = document.getElementsByTagName('table')[0].children[1].children[0].children[0].textContent;
 var listOfTable = document.getElementsByTagName('table');
+if(listOfTable[i].children[0])
 var elementsCount = 0;
 for(var i=0;i<listOfTable.length;i++) {
-    var singleTableCount = listOfTable[i].children[1].childElementCount;
-elementsCount = elementsCount + singleTableCount;
-//console.log(elementsCount);
+    if(listOfTable[i].childElementCount > 1){
+        elementsCount = elementsCount + listOfTable[i].children[1].childElementCount;
+    }
+    else if(listOfTable[i] === 1){
+        elementsCount = elementsCount + listOfTable[i].children[0].childElementCount -1
+    }
+//Console.log(elementsCount);
 }
 console.log(elementsCount);
+//document.getElementsByTagName('table')[2].children[0].childElementCount;
+
 
 
 
