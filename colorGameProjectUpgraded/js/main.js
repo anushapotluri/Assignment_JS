73e6bf5c1c272a3cabcd1d70d6696d7688a2660e
Anusha <potluri.anusha1@gmail.com>
2019-12-13 08:04:52 +0530

    color Finished

--- colorGameProjectUpgraded/js/main.js
+++ colorGameProjectUpgraded/js/main.js
@@ -3,11 +3,24 @@ var colorBoxesContainer = document.getElementById('color-boxes-container');
 window.onload = resetGame();
 var newGame= document.getElementById('new-color');
 newGame.addEventListener('click',resetGame);
-document.getElementById('easy').addEventListener('click', function(){
+document.getElementById('easy').addEventListener('click', function()
+{   
+     if(numberOfColorBoxes !== 3){
     changeLevel(3);
+    }
+    else{
+        alert("already in easy level");
+    }
 });
-document.getElementById('hard').addEventListener('click', function(){
+
+document.getElementById('hard').addEventListener('click', function()
+{
+    if(numberOfColorBoxes !== 6){
     changeLevel(6);
+    }
+    else{
+        alert("already in hard level");
+    }
 });
 
 
